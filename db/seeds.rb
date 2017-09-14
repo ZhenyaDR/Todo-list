c6774ef713d3621904f0b197f8d17255bf5a4536
Dreyko Evgeniy <drejko.ev@yandex.ru>
2017-09-14 21:08:50 +0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-	
+	seed_file = Rails.root.join('db', 'seeds', 'seeds.yml')
+config = YAML::load_file(seed_file)
+Project.create!(config)
